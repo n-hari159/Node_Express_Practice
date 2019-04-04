f8b632805ae07805ecb843bd5663a17f58225d79
n-hari159 <n.hari159@gmail.com>
2019-04-05 04:23:38 +0530

    Working with static files and Middleware

--- express1/app.js
+++ express1/app.js
@@ -3,8 +3,17 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+// Middleware
+app.use('/assets', express.static(__dirname + '/public'));
+
+// Creating own Middleware
+app.use('/', function(req, res, next) {
+    console.log('Request Url: ' + req.url);
+    next();
+});
+
 app.get('/', function(req, res) {
-    res.send('<html><head></head><body><h1>Hello</h1></body></html>');
+    res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello</h1></body></html>');
 });
 
 app.get('/api', function(req, res) {
